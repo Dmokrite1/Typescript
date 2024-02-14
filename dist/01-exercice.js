cffd738a15cb6a8a589c0bd48b2bbc3a7719b40e
Dmokrite1 <hg_greg@proton.me>
2024-02-14 13:59:42 +0100

    add .join(' et ') at the types Animals

--- dist/01-exercice.js
+++ dist/01-exercice.js
@@ -13,6 +13,6 @@ const monChat = {
 function faireManger(animaux) {
     const animalsName = animaux.map(animal => animal.name);
     const typesAnimal = animaux.map(animal => animal.type).join(' et ');
-    console.log(`${animalsName.join(' et ')} qui sont de type ${typesAnimal} sont en train de mangés.`);
+    console.log(`${animalsName.join(' et ')} qui sont de type ${typesAnimal.join(' et ')} sont en train de mangés.`);
 }
 faireManger([monChien, monChat]);
