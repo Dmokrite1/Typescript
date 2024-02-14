4a228078e9485e0bc54e830cf56603ae4c683921
Dmokrite1 <hg_greg@proton.me>
2024-02-14 14:00:53 +0100

    add empty lines for reading comfort

--- dist/01-exercice.js
+++ dist/01-exercice.js
@@ -5,14 +5,17 @@ class Chien {
         this.type = "canin";
     }
 }
+
 const monChien = new Chien("Machin");
 const monChat = {
     name: 'Truc',
     type: "félin",
 };
+
 function faireManger(animaux) {
     const animalsName = animaux.map(animal => animal.name);
     const typesAnimal = animaux.map(animal => animal.type).join(' et ');
     console.log(`${animalsName.join(' et ')} qui sont de type ${typesAnimal.join(' et ')} sont en train de mangés.`);
 }
+
 faireManger([monChien, monChat]);
